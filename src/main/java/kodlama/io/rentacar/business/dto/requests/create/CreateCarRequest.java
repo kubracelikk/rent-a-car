cb05ac4c7576abe21835747582f30d7428de6dc2
Kubra Celik <kubracelikce@gmail.com>
2023-04-25 00:27:32 +0300

    added plate control

--- src/main/java/kodlama/io/rentacar/business/dto/requests/create/CreateCarRequest.java
+++ src/main/java/kodlama/io/rentacar/business/dto/requests/create/CreateCarRequest.java
@@ -1,6 +1,8 @@
 package kodlama.io.rentacar.business.dto.requests.create;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import kodlama.io.rentacar.common.constants.Regex;
 import kodlama.io.rentacar.common.utils.annotations.NotFutureYear;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,6 +18,7 @@ public class CreateCarRequest {
     //@Max(2023)
     @NotFutureYear //custom annotation
     private int modelYear;
+    @Pattern(regexp = Regex.Plate, message = "Invalid license plate")
     private String plate;
     @Min(1)
     private double dailyPrice;
