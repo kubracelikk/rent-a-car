38b2a79b651e1c630b0eecd219e0b44a3e29855d
Kubra Celik <kubracelikce@gmail.com>
2023-05-04 20:45:03 +0300

    add caching

--- src/main/java/kodlama/io/rentacar/RentACarApplication.java
+++ src/main/java/kodlama/io/rentacar/RentACarApplication.java
@@ -2,9 +2,8 @@ package kodlama.io.rentacar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
+
 @SpringBootApplication
 public class RentACarApplication {
 
--- src/main/java/kodlama/io/rentacar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentacar/business/concretes/BrandManager.java
@@ -12,8 +12,6 @@ import kodlama.io.rentacar.entities.Brand;
 import kodlama.io.rentacar.repository.BrandRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,7 +24,6 @@ public class BrandManager implements BrandService {
     private final BrandBusinessRules rules;
 
     @Override
-    @Cacheable(value = "brand_list")
     public List<GetAllBrandsResponse> getAll() {
         List<Brand> brands = repository.findAll();
         List<GetAllBrandsResponse> response = brands
@@ -45,7 +42,6 @@ public class BrandManager implements BrandService {
     }
 
     @Override
-    @CacheEvict(value = "brand_list", allEntries = true)
     public CreateBrandResponse add(CreateBrandRequest request) {
         rules.checkIfBrandExistsByName(request.getName());
         //Brand brand = new Brand();
